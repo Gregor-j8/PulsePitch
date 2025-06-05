89515fcbf95fcacbf222ec479f0d7bcf5a908d70
gregor-J8 <gregor.johnson@outlook.com>
2025-06-05 18:54:29 -0500

    added bounding box

--- AI/main.py
+++ AI/main.py
@@ -1,8 +1,16 @@
 from utils.video_utils import read_video, save_video
+from trackers.tracker import Tracker
 
 def main():
     video_frames = read_video('data/italyGame.mp4')
-    save_video(video_frames, 'output_videos/output_video.avi')
+    
+    tracker = Tracker('models/best.pt')
+    tracks = tracker.get_object_tracks(video_frames, read_from_stub=True, stub_path='stubs/track_stubs.pkl')
+    
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    
+    save_video(output_video_frames, 'output_videos/output_video.avi')
+    
 
 if __name__ == '__main__':
     main()
