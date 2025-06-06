13202f4c2142fc80dddcb67deb081d45396cac2e
gregor-J8 <gregor.johnson@outlook.com>
2025-06-06 09:07:41 -0500

    added circles for tracking

--- AI/trackers/tracker.py
+++ AI/trackers/tracker.py
@@ -1,6 +1,7 @@
 from ultralytics import YOLO
 import supervision as sv
 import pickle
+import numpy as np
 import os
 import sys
 sys.path.append('../')
@@ -73,7 +74,7 @@ class Tracker:
              
         return tracks
     
-    def draw_ellipse(self, frame, bbox, color, track_id):
+    def draw_ellipse(self, frame, bbox, color, track_id=None):
         y2 = int(bbox[3])
         
         x_center, _ = get_center_of_bbox(bbox)
@@ -90,6 +91,47 @@ class Tracker:
             thickness=2,
         )
         
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - rectangle_width//2
+        x2_rect = x_center + rectangle_width//2
+        y1_rect = (y2 - rectangle_height//2) + 15
+        y2_rect = (y2 + rectangle_height//2) + 15
+        
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect), int(y1_rect)),
+                          (int(x2_rect), int(y2_rect)),
+                          color,
+                          cv2.FILLED)
+            
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -= 10
+                
+            cv2.putText(frame, 
+                        f"{track_id}",
+                        (int(x1_text), int(y1_rect+15)),
+                        cv2.FONT_HERSHEY_COMPLEX,
+                        0.6,
+                        (0, 0 ,0),
+                        2)
+        
+        return frame
+    
+    
+    def draw_triangle(self, frame, bbox, color):
+        y = int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
+        
+        triangle_points = np.array([
+            [x,y],
+            [x-10,y-20],
+            [x+10,y+20],
+        ])
+        cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points], 0, (0,0,0), 2)
+        # cv2.drawChessboardCorners()
         return frame
         
     def draw_annotations(self, video_frames, tracks):
@@ -103,8 +145,11 @@ class Tracker:
             
             for track_id, player in player_dict.items():
                 frame = self.draw_ellipse(frame, player["bbox"], (0, 0, 255), track_id)
-                
-                
+            for _, referee in referees_dict.items():
+                frame = self.draw_ellipse(frame, referee["bbox"], (0, 255, 255))
+            for track_id, ball in ball_dict.items():
+                frame = self.draw_triangle(frame, ball["bbox"], (0, 255, 0))
+
             output_video_frames.append(frame)
             
         return output_video_frames
