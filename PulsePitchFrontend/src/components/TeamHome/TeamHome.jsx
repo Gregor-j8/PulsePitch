4e3c37df581acf2e7f94000e2b32bce961a468c1
gregor-J8 <gregor.johnson@outlook.com>
2025-06-16 17:50:22 -0500

    added user profile to frontend

--- PulsePitchFrontend/src/components/TeamHome/TeamHome.jsx
+++ PulsePitchFrontend/src/components/TeamHome/TeamHome.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import { useDeletePlayerTeam, useGetPlayersFromTeam } from "../../hooks/usePlayerTeams";
 
 export const TeamHome = () => {
@@ -9,7 +10,7 @@ export const TeamHome = () => {
     <div className="flex flex-col items-center justify-center h-full pt-40">
          {teams?.map(team => (
              <div key={team.id} className="flex justify-between bg-white shadow-md rounded-lg p-6 mb-4 w-1/2">
-                <h2 className="text-xl font-bold mb-2">{team?.player.firstName} {team?.player.lastName}</h2>
+                <h2 className="text-xl font-bold mb-2"><Link to={`/profile/${team.id}`}>{team?.player.firstName} {team?.player.lastName}</Link></h2>
                 <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600" onClick={() => deletePlayerTeam(team.id)}>Delete</button>
             </div>
         ))}
---
+++ PulsePitchFrontend/src/managers/UserProfile.js
@@ -0,0 +1,15 @@
+import axiosClient from './axiosClient'
+
+export const fetchUserProfileById = async (id) => {
+  const { data } = await axiosClient.get(`/userprofile/${id}`)
+  return data
+}
+
+export const editUserProfile = async (id, userprofileData) => {
+  const { data } = await axiosClient.patch(`/userprofile/${id}`, userprofileData)
+  return data
+}
+
+export const deleteUserProfile = async (id) => {
+  await axiosClient.delete(`/userprofile/${id}`)
+}
