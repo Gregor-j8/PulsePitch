8c4e38a4eae436c79dad9f4904d914afa6eb3c20
gregor-J8 <gregor.johnson@outlook.com>
2025-06-25 17:36:37 -0500

    added react toastify

--- PulsePitchFrontend/src/main.jsx
+++ PulsePitchFrontend/src/main.jsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import "./index.css"
+import { ToastContainer } from 'react-toastify'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient()
 
@@ -9,6 +10,7 @@ createRoot(document.getElementById('root')).render(
   <BrowserRouter>
       <QueryClientProvider client={queryClient}>
           <App />
-        </QueryClientProvider>
+          <ToastContainer />
+      </QueryClientProvider>
   </BrowserRouter>
 );
